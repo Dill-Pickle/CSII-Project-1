4df4369f1dabe2d51e12ea851bffb4aaaae76b47
Jordan Stormo <jordanstormo@yahoo.com>
2015-07-07 19:05:17 -0500

    Create Rand Number.cpp

---
+++ Rand Number.cpp
@@ -0,0 +1,72 @@
+/*Jordan Stormo
+  Hasani Mason
+  Devin Gonzolas
+*/
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+using namespace std;
+
+void numbers();
+void cards();
+
+void main()
+{
+	srand(time(NULL));
+	numbers();
+	cards();
+
+}
+void numbers()
+{
+	char answer;
+	int numb[9];
+
+	do
+	{
+		for (int i = 1; i < 9; i++)
+		{
+			numb[i] = rand() % 52;
+
+			if (numb[i] == numb[i - 1])
+			{
+				numb[i] = rand() % 52;
+			}
+		}
+		for (int i = 0; i < 9; i++)
+		{
+			
+			cout << numb[i] << endl;
+		}
+		
+		cout << "Again (y/n)";
+		cin >> answer;
+		cout << endl;
+	}
+	while (answer == 'y');
+}
+void cards()
+{
+	char ans;
+	int N;
+	int S;
+	int C;
+	int check[9];
+	string suit[4] = { "Hearts", "Clubs", "Diamonds", "Spade" };
+	string card[13] = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+	do
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			N = rand() % 52;
+			S = rand() % 4;
+			C = rand() % 13;
+
+			cout << card[C] << " of " << suit[S] << endl;
+		}
+		cout << "Again? (y/n)";
+		cin >> ans;
+	} while (ans == 'y');
+}
